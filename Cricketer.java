80b4740bbc5eca25f8eb0d5b6288352edae2b37e
rafat1515 <113086563+rafat1515@users.noreply.github.com>
2022-09-26 16:35:54 +0530

    Add files via upload

---
+++ Cricketer.java
@@ -0,0 +1,16 @@
+package corejava_basic1;
+
+public class Cricketer extends Players
+{
+	void bat(int score)
+	{
+		System.out.println("Score is: " + score);
+	}
+	
+	void ball(String team)
+	{
+		System.out.println("Team name is: " +team);
+	}
+	
+
+}
---
+++ Footballer.java
@@ -0,0 +1,20 @@
+package corejava_basic1;
+
+public class Footballer  extends Players
+{
+	void goal(int score)
+	
+	{
+		System.out.println("Score is: " + score);
+	}
+	
+	void defence(String name)
+	{
+		System.out.println("Team name is: " + name);
+	}
+	
+	
+	
+	
+	
+}
